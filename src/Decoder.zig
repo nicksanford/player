bddec39f0ad2da83593c561297583a3e4bccd87f
Nick Sanford <nicholascsanford@gmail.com>
2026-02-15 23:37:41 -0500

    wip

--- src/Decoder.zig
+++ src/Decoder.zig
@@ -54,8 +54,84 @@ pub fn deinit(self: *Self) void {
     self.rgba_frame.free();
 }
 
-test "nick init" {
-    std.testing.log_level = .debug;
-    var s = try Self.init("/home/user/Downloads/bbb_sunflower_2160p_60fps_normal.mp4");
-    s.deinit();
+fn scaleMaintainingAspectRatio(src_width: u32, src_height: u32, target_width: u32, target_height: u32) struct { u32, u32 } {
+    const fsrc_width: f32 = @as(f32, @floatFromInt(src_width));
+    const fsrc_height: f32 = @as(f32, @floatFromInt(src_height));
+    const fwindow_width: f32 = @as(f32, @floatFromInt(target_width));
+    const fwindow_height: f32 = @as(f32, @floatFromInt(target_height));
+    if (src_width <= target_width and src_height <= target_height) {
+        // no need to scale as the window is greater in both
+        // dimensions
+        return .{ src_width, src_height };
+    }
+
+    const src_aspect_ratio: f32 = fsrc_width / fsrc_height;
+    const window_aspect_ratio: f32 = fwindow_width / fwindow_height;
+    var output_width: f32 = undefined;
+    var output_height: f32 = undefined;
+    if (src_width >= src_height) {
+        // horizontal
+        if (src_aspect_ratio >= window_aspect_ratio) {
+            output_width = @divFloor(fwindow_width, 2) * 2;
+            output_height = @divFloor(fwindow_width * (fsrc_height / fsrc_width), 2) * 2;
+        } else {
+            output_width = @divFloor(fwindow_height * (fsrc_width / fsrc_height), 2) * 2;
+            output_height = @divFloor(fwindow_height, 2) * 2;
+        }
+
+        return .{ @intFromFloat(output_width), @intFromFloat(output_height) };
+    }
+    // TODO: vertical video unimplemented
+    unreachable;
 }
+
+fn f(x: u32) struct { u32, u32 } {
+    return .{ x, x };
+}
+test "f" {
+    const Test = struct {
+        x: u32,
+        out1: u32,
+        out2: u32,
+    };
+    const tests = [_]Test{
+        .{ .x = 1, .out1 = 1, .out2 = 1 },
+    };
+
+    for (tests) |t| {
+        try std.testing.expectEqual(f(t.x), struct { u32, u32 }{ t.out1, t.out2 });
+    }
+}
+
+test "fitWindow" {
+    const Test = struct {
+        src_width: u32,
+        src_height: u32,
+        window_width: u32,
+        window_height: u32,
+        output_width: u32,
+        output_height: u32,
+    };
+    const tests = [_]Test{
+        // equal
+        .{ .src_width = 3840, .src_height = 2160, .window_width = 3840, .window_height = 2160, .output_width = 3840, .output_height = 2160 },
+        // media smaller than window
+        .{ .src_width = 3840, .src_height = 2160, .window_width = 4000, .window_height = 2160, .output_width = 3840, .output_height = 2160 },
+        .{ .src_width = 3840, .src_height = 2160, .window_width = 3840, .window_height = 3000, .output_width = 3840, .output_height = 2160 },
+        .{ .src_width = 3840, .src_height = 2160, .window_width = 4000, .window_height = 3000, .output_width = 3840, .output_height = 2160 },
+        // media larger than window
+        .{ .src_width = 3840, .src_height = 2160, .window_width = 1280, .window_height = 720, .output_width = 1280, .output_height = 720 },
+        .{ .src_width = 3840, .src_height = 2160, .window_width = 1280, .window_height = 800, .output_width = 1280, .output_height = 720 },
+        .{ .src_width = 3840, .src_height = 2160, .window_width = 2000, .window_height = 720, .output_width = 1280, .output_height = 720 },
+        .{ .src_width = 3840, .src_height = 2160, .window_width = 2001, .window_height = 1000, .output_width = 1776, .output_height = 1000 },
+    };
+
+    for (tests) |t| {
+        try std.testing.expectEqual(.{ t.output_width, t.output_height }, scaleMaintainingAspectRatio(t.src_width, t.src_height, t.window_width, t.window_height));
+    }
+}
+// test "nick init" {
+//     std.testing.log_level = .debug;
+//     var s = try Self.init("/home/user/Downloads/bbb_sunflower_2160p_60fps_normal.mp4");
+//     s.deinit();
+// }
